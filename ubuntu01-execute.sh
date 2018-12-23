49458054db8b620a2fc9bf6c658adb05940d57b0
ErikVaBu <evanbuggenhout@nviso.be>
2018-12-23 14:27:07 +0100

    Update ubuntu01-execute.sh

--- ubuntu01-execute.sh
+++ ubuntu01-execute.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# SANS SEC599 Hot Fix Script
+# Only to be used for quick fixes
+# Author: Erik Van Buggenhout (evanbuggenhout@nviso.be)
+# Last revision: 23 December 2018
 
+ifconfig eth0 10.10.10.80
+service networking restart
