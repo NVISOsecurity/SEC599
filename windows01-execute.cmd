8a94d876d7e056ee65794f79c26a4bebde3efce4
ErikVaBu <evanbuggenhout@nviso.be>
2019-01-30 18:15:04 +0400

    Update windows01-execute.cmd

--- windows01-execute.cmd
+++ windows01-execute.cmd
@@ -2,5 +2,3 @@ REM SEC599 Maintenance script for Windows01
 REM ---------------------------------------
 REM Last update: 30 January 2019
 REM This script is used for emergency patches / updates in SEC599
-
-net user john Password123 /add
