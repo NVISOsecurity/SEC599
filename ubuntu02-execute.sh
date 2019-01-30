8060483a821a2bba09ed3a62433e0d1fd6a81bd3
ErikVaBu <evanbuggenhout@nviso.be>
2019-01-30 17:20:05 +0400

    Update ubuntu02-execute.sh

--- ubuntu02-execute.sh
+++ ubuntu02-execute.sh
@@ -1,1 +1,6 @@
+# SEC599 Maintenance script for Ubuntu02
+# --------------------------------------
+# Last update: 30 January 2019
+# This script is used for emergency patches / updates in SEC599
 
+whoami > /tmp/whoami
