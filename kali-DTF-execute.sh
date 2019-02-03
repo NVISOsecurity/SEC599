ab68bd3adef2d587e28ba2fe497e7945f48bbe38
ErikVaBu <evanbuggenhout@nviso.be>
2019-02-03 18:31:54 +0100

    Update kali-DTF-execute.sh

--- kali-DTF-execute.sh
+++ kali-DTF-execute.sh
@@ -1,6 +1,4 @@
-# SEC599 Maintenance script for Kali
-# ----------------------------------
+# SEC599 Maintenance script for Kali DTF
+# --------------------------------------
 # Last update: 30 January 2019
 # This script is used for emergency patches / updates in SEC599
-
-whoami > /tmp/whoami.txt
