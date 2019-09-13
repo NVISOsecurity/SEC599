139e4205f08b7c172bc240ec162875189eba7c27
Erik Van Buggenhout <evanbuggenhout@nviso.be>
2019-09-13 15:22:57 -0700

    Update kali-DTF-execute.sh

--- kali-DTF-execute.sh
+++ kali-DTF-execute.sh
@@ -6,3 +6,11 @@
 # Issue description: A recent Defender update triggers on the following exact commandline combination: "privilege::debug sekurlsa::logonPasswords"
 # Solution: Use PowerShell to avoid "privilege::debug" (PowerShell already has debug privilege)
 
+/usr/bin/wget -O /root/Desktop/CTF/4-movetowin01-INITIAL.sh https://raw.githubusercontent.com/NVISO-BE/SEC599/master/4-movetowin01-INITIAL.sh
+/usr/bin/wget -O /root/Desktop/CTF/4-movetowin01-FINAL.sh https://raw.githubusercontent.com/NVISO-BE/SEC599/master/4-movetowin01-FINAL.sh
+
+/usr/bin/wget -O /root/Desktop/CTF/5-movetowin02-INITIAL.sh https://raw.githubusercontent.com/NVISO-BE/SEC599/master/5-movetowin02-INITIAL.sh
+/usr/bin/wget -O /root/Desktop/CTF/5-movetowin02-FINAL.sh https://raw.githubusercontent.com/NVISO-BE/SEC599/master/5-movetowin02-FINAL.sh
+
+/usr/bin/wget -O /root/Desktop/CTF/6-movetodc-INITIAL.sh https://raw.githubusercontent.com/NVISO-BE/SEC599/master/4-movetodc-INITIAL.sh
+/usr/bin/wget -O /root/Desktop/CTF/6-movetodc-FINAL.sh https://raw.githubusercontent.com/NVISO-BE/SEC599/master/6-movetodc-FINAL.sh
